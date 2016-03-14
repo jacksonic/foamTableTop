9fec2ba7e653fe2cb3da5cfdb82a89deced8ebb4
Jackson Stone <jacksonic@google.com>
2016-03-14 15:53:01 -0400

    SpatialDAO mlang optimization implementation in progress

--- src/dao/SpatialDAO.js
+++ src/dao/SpatialDAO.js
@@ -16,8 +16,8 @@
  */
 
 
-/** Ternary expression for bounds check of the first argument within the
- bounds given by the second argument array. */
+/** Binary expression for bounds check of the first argument within the
+ range given by the second argument (an array of [min, max]). */
 foam.CLASS({
   package: 'foam.mlang.predicate',
   name: 'Bounded',
@@ -25,8 +25,8 @@ foam.CLASS({
   methods: [
     function f(o) {
       return
-        ( foam.util.compare(this.arg1.f(o), this.arg2.f(o)[0]) <= 0 ) &&
-        ( foam.util.compare(this.arg1.f(o), this.arg2.f(o)[1]) >= 0 );
+        ( foam.util.compare(this.arg1.f(o), this.arg2.f(o)[0]) >= 0 ) &&
+        ( foam.util.compare(this.arg1.f(o), this.arg2.f(o)[1]) <= 0 );
     }
   ]
 });
@@ -47,7 +47,12 @@ foam.CLASS({
   requires: [
     'foam.dao.ArraySink',
     'foam.mlang.predicate.True',
-    'foam.mlang.predicate.Bounded'
+    'foam.mlang.predicate.Bounded',
+    'foam.mlang.predicate.Eq',
+    'foam.mlang.predicate.Lt',
+    'foam.mlang.predicate.Gt',
+    'foam.mlang.predicate.Lte',
+    'foam.mlang.predicate.Gte'
   ],
 
   properties: [
@@ -63,20 +68,39 @@ foam.CLASS({
     },
     {
       /** The size of each bucket (effectively divide each coordinate by this
-        value and round off to get the hash) */
+        value and round off to get the hash). Changing this value invalidates
+        the existing buckets. */
       name: 'bucketWidth',
       defaultValue: 10,
     },
   ],
 
   methods: [
-    /** A default hash for an object with an x, y, width, height.
-      Returns an array of keys for the buckets the object should occupy. */
-    function hashXY_(obj /* array */) {
+    /** A default hash for any object with an x, y, width, height.
+      Returns an array of buckets the object should occupy. if createMode is
+      true, buckets will be created if not present. */
+    function hashXY_(bounds, createMode /* array */) {
       var ret = [];
+      var bw = this.bucketWidth;
 
+      // if infinite area, don't try to filter (not optimal: we might only
+      // want half, but this data structure is not equipped for space partitioning)
+      if ( bounds.width === Infinity || bounds.height === Infinity ) {
+        return this.buckets;
+      }
 
-
+      for ( var w = 0; w < ( obj.width || 0.000001 ); w += bw ) {
+        for ( var h = 0; h < ( obj.height || 0.000001 ); h += bw ) {
+          var key = "x" +
+            Math.floor( ( obj.x + w ) / bw ) * bw + "y" +
+            Math.floor( ( obj.y + h ) / bw ) * bw;
+          var bucket = this.buckets[key];
+          if ( ( ! bucket ) && createMode ) {
+            bucket = this.buckets[key] = {};
+          }
+          bucket && ret.push(bucket);
+        }
+      }
       ret.object = obj;
       return ret;
     },
@@ -90,10 +114,10 @@ foam.CLASS({
       this.remove(obj); // TODO: don't remove from buckets it's going back into
 
       // add to the buckets the item overlaps
-      var buckets = this.hashXY_(obj);
+      var buckets = this.hashXY_(obj, true);
       this.items[obj.id] = buckets; // for fast removal later
       for (var i = 0; i < buckets.length; ++i) {
-        this.buckets[buckets[i]][obj.id] = obj;
+        buckets[i][obj.id] = obj;
       }
 
       sink.put(obj);
@@ -107,7 +131,7 @@ foam.CLASS({
 
       var buckets = this.items[obj.id];
       for (var i = 0; i < buckets.length; ++i) {
-        delete this.buckets[buckets[i]][obj.id];
+        delete buckets[i][obj.id];
         // TODO: delete empty buckets
       }
 
@@ -133,20 +157,29 @@ foam.CLASS({
       // limit result set if BOUNDED filter is found
 //////////////////////// from MDAO
       var items = this.items;
-      var query = options.where;
+      var query = options.where.clone();
 
-      var propX = this.of.getAxiomByName('x');
-      var propY = this.of.getAxiomByName('y');
+      var axes = { x: true, y: true };
       var isIndexed = function(mlangArg) {
-        return mlangArg === propX || mlangArg === propY;
+        // any old x and y will do
+        // TODO: these will be configurable
+        return !! axes[mlangArg.name];
       }
 
+      // TODO: make sure a top level OR is handled
+
+      // Actually want to grab all nested bounds and filter buckets based
+      // on all of them... the intersection for AND, and the
+      // union for OR.
+      // In the AND/intersection case, we want to know all the bounds together and
+      // do the search once, since any unspecified bound will catch lots of
+      // buckets.
       var isExprMatch = function(model) {
         if ( ! model ) return undefined;
 
         if ( query ) {
 
-          if ( model.isInstance(query) && query.arg1 === prop ) {
+          if ( model.isInstance(query) && isIndexed(query.arg1)  ) {
             var arg2 = query.arg2;
             query = undefined;
             return arg2;
@@ -160,7 +193,7 @@ foam.CLASS({
                 query.args[i] = this.True;
                 query = query.partialEval();
                 if ( query === this.True ) query = null;
-                return q.arg2;
+                return q;
               }
             }
           }
@@ -168,21 +201,74 @@ foam.CLASS({
         return undefined;
       };
 
-      var arg2 = isExprMatch(this.Bounded);
-      if ( arg2 ) {
-        // filter out the buckets not in the bounded range
-
+      // accumulate range limits so we can make as specific query as possible
+      var ranges = {
+        x: [-Infinity, Infinity],
+        y: [-Infinity, Infinity]
       }
-////////////////////////
 
+      var args;
+      // Each hit of isExprMatch will pick off one thing ANDed at the top
+      // level. Since all these bounds apply at once, keep shrinking the
+      // search bounds.
+      // TODO: use compare instead of Math.min, to allow for non-number ranges
+      while ( args = isExprMatch(this.Bounded) ) {
+        // accumulate the bounds (largest minimum, smallest maximum)
+        ranges[args.arg1.name][0] = Math.max( ranges[args.arg1.name][0], args.arg2[0] );
+        ranges[args.arg1.name][1] = Math.min( ranges[args.arg1.name][1], args.arg2[1] );
+      }
+
+      // Equals will completely restrict one axis to a zero-width range (one value)
+      while ( args = isExprMatch(this.Eq) ) {
+        // accumulate the bounds (largest minimum, smallest maximum)
+        ranges[args.arg1.name][0] = args.arg2;
+        ranges[args.arg1.name][1] = args.arg2;
+      }
+
+      // Less than restricts the maximum for an axis
+      while ( args = isExprMatch(this.Lte) ) {
+        // accumulate the bounds(smallest maximum)
+        ranges[args.arg1.name][1] = Math.min( ranges[args.arg1.name][1], args.arg2 );
+      }
+      while ( args = isExprMatch(this.Lt) ) {
+        // accumulate the bounds(smallest maximum)
+        ranges[args.arg1.name][1] = Math.min( ranges[args.arg1.name][1], args.arg2 );
+      }
+
+      // Greater than restricts the minimum for an axis
+      while ( args = isExprMatch(this.Gte) ) {
+        // accumulate the bounds(smallest maximum)
+        ranges[args.arg1.name][0] = Math.max( ranges[args.arg1.name][0], args.arg2 );
+      }
+      while ( args = isExprMatch(this.Gt) ) {
+        // accumulate the bounds(smallest maximum)
+        ranges[args.arg1.name][0] = Math.max( ranges[args.arg1.name][0], args.arg2 );
+      }
+
+////////////////////////
       var fc = this.FlowControl.create();
-      for ( var key in items ) {
-        if ( fc.stopped ) break;
-        if ( fc.errorEvt ) {
-          sink.error(fc.errorEvt);
-          return Promise.reject(fc.errorEvt);
+
+      // if bounds end up infinite, hash will return all buckets, forcing all
+      // items through the predicatedSink by default.
+      var bounds = {};
+      bounds.x = ranges.x[0];
+      bounds.y = ranges.y[1];
+      bounds.width = ranges.x[1] - ranges.x[0];
+      bounds.height = ranges.y[1] - ranges.y[0];
+
+      var buckets = this.hashXY_(bounds);
+      for ( var i = 0; ( i < buckets.length ) && ! fc.stopped; ++i ) {
+        for ( var key in buckets[i] ) {
+          var obj = buckets[i][key];
+          if ( obj.id ) {
+            if ( fc.stopped ) break;
+            if ( fc.errorEvt ) {
+              sink.error(fc.errorEvt);
+              return Promise.reject(fc.errorEvt);
+            }
+            sink.put(obj, null, fc);
+          }
         }
-        sink.put(this.items[key].object, null, fc);
       }
 
       sink && sink.eof && sink.eof();
