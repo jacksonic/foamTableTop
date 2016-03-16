e659c3d0b97ed48470a8f158cf9f8c3c95517339
Jackson Stone <jacksonic@google.com>
2016-03-16 11:15:55 -0400

    Basic entity classes added

---
+++ src/Entities.js
@@ -0,0 +1,96 @@
+/*
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+  Base class for entities. Includes world position, rendering information, etc.
+*/
+foam.CLASS({
+  package: 'tabletop',
+  name: 'Entity',
+  properties: [
+    { /** World coords */
+      name: 'x'
+    },
+    { /** World coords */
+      name: 'y'
+    },
+    { /** x velocity, world units/sec */
+      name: 'vx'
+    },
+    { /** y velocity, world units/sec */
+      name: 'vy'
+    },
+    { /** World relative */
+      name: 'angle'
+    },
+    {
+      /** The EntityManager that owns this entity. */
+      name: 'manager'
+    },
+    {
+      /** The renderable Sprite for this entity. */
+      name: 'sprite'
+    },
+  ],
+});
+
+/**
+  Base class for entity Sprites. Sprites can form trees, so a single
+  Sprite for an Entity may have many sub-sprites inside, which the entity
+  may control (turrets, for instance)
+*/
+foam.CLASS({
+  package: 'tabletop',
+  name: 'Sprite',
+  properties: [
+    { /** Rendering coords */
+      name: 'x'
+    },
+    { /** Rendering coords */
+      name: 'y'
+    },
+    { /** Parent sprite relative */
+      name: 'angle'
+    },
+    {
+      /** The entity that owns this sprite. */
+      name: 'entity'
+    },
+    // TODO: image, offset....
+  ],
+});
+
+/**
+  Base class for a thing that manages a group of entities. Such things include
+  human players, computer enemy waves, and bullet groups.
+*/
+foam.CLASS({
+  package: 'tabletop',
+  name: 'EntityManager',
+  requires: [
+    'tabletop.Entity',
+  ],
+  properties: [
+    {
+      /** The group of managed entities */
+      name: 'entities',
+      class: 'foam.core.Array',
+      of: 'tabletop.Entity',
+    }
+  ],
+});
+
