088587152bb0ad32900a632539d24be6b4f2d16d
Jackson Stone <jacksonic@google.com>
2016-03-18 16:10:19 -0400

    Player added

--- src/GameRunner.js
+++ src/GameRunner.js
@@ -28,9 +28,11 @@ foam.CLASS({
     'foam.mlang.sink.Map',
     'tabletop.Physics',
     'tabletop.Audio',
+    'tabletop.PlayerManager',
   ],
   exports: [
     'worldDAO',
+    'canvas',
     'time',
   ],
   properties: [
@@ -48,92 +50,105 @@ foam.CLASS({
     },
     {
       name: 'canvas',
-      factory: function() {
-        var c = this.Canvas.create({
-          width:  document.body.clientWidth,
-          height: document.body.clientHeight,
-          id: 'game-runner'
-        });
-        c.cview = this.CView.create();
-        return c;
-      }
     },
     {
       name: 'time',
       defaultValue: 0
+    },
+    {
+      name: 'players',
+      factory: function() {
+        return [
+          this.PlayerManager.create({ corner: [0,0] }),
+          this.PlayerManager.create({ corner: [0,1] }),
+          this.PlayerManager.create({ corner: [1,0] }),
+          this.PlayerManager.create({ corner: [1,1] })
+        ];
+      }
     }
   ],
   methods: [
     function init() {
-      // create test entities
-      var x, y;
-      var spacing = 80;
-      for (var k = 0; k < 100; ++k) {
-//        x = Math.random() * this.canvas.width;
-//        y = Math.random() * this.canvas.height;
-        if (Math.random() > 0.8) {
-          this.worldDAO.put(this.TestEntity.create({
-            id: 'test'+k,
-            x: (k%10) * spacing,
-            y: Math.floor(k/10) * spacing,
-            br: 20,
-            ax: Math.random() * 10 - 5,
-            ay: Math.random() * 10 - 5,
-            arotation: Math.random() * 0.02 - 0.01,
-            vx: Math.random() * 10 - 5,
-            vy: Math.random() * 10 - 5,
-          }));
-        } else {
-          this.worldDAO.put(this.TestEntity.create({
-            id: 'test'+k,
-            x: (k%10) * spacing,
-            y: Math.floor(k/10) * spacing,
-            br: 20,
-          }));
-        }
-      }
-
-      this.worldDAO.put(this.TestEntity.create({
-        id: 'testbullet',
-        x: 2000,
-        y: 300,
-        br: 20,
-        ax: -20,
-      }));
-      this.worldDAO.put(this.TestEntity.create({
-        id: 'testbullet2',
-        x: 4000,
-        y: 500,
-        br: 20,
-        ax: -20,
-      }));
-      this.worldDAO.put(this.TestEntity.create({
-        id: 'testbullet3',
-        x: 6000,
-        y: 400,
-        br: 20,
-        ax: -20,
-      }));
-      this.worldDAO.put(this.TestEntity.create({
-        id: 'testbullet4',
-        x: 7000,
-        y: 200,
-        br: 20,
-        ax: -20,
-      }));
-      this.worldDAO.put(this.TestEntity.create({
-        id: 'testbullet5',
-        x: 8000,
-        y: 300,
-        br: 20,
-        ax: -20,
-      }));
-
-
-      // make views
-      var views = this.ArraySink.create();
-      this.worldDAO.select(this.Map.create({ f: this.Entity.SPRITE.f, delegate: views }));
-      this.canvas.cview.children = views.a;
+      // for now, avoid infinite recursion (exported thing with factory that creates something that uses context)
+      this.canvas = this.Canvas.create({
+        width:  document.body.clientWidth,
+        height: document.body.clientHeight,
+        id: 'game-runner'
+      }, foam.X);
+      this.canvas.cview = this.CView.create();
+      
+      this.players;
+      
+//
+//       // create test entities
+//       var x, y;
+//       var spacing = 80;
+//       for (var k = 0; k < 10; ++k) {
+// //        x = Math.random() * this.canvas.width;
+// //        y = Math.random() * this.canvas.height;
+//         if (Math.random() > 0.8) {
+//           this.worldDAO.put(this.TestEntity.create({
+//             id: 'test'+k,
+//             x: (k%10) * spacing,
+//             y: Math.floor(k/10) * spacing,
+//             br: 20,
+//             ax: Math.random() * 10 - 5,
+//             ay: Math.random() * 10 - 5,
+//             arotation: Math.random() * 0.02 - 0.01,
+//             vx: Math.random() * 10 - 5,
+//             vy: Math.random() * 10 - 5,
+//           }));
+//         } else {
+//           this.worldDAO.put(this.TestEntity.create({
+//             id: 'test'+k,
+//             x: (k%10) * spacing,
+//             y: Math.floor(k/10) * spacing,
+//             br: 20,
+//           }));
+//         }
+//       }
+//
+//       this.worldDAO.put(this.TestEntity.create({
+//         id: 'testbullet',
+//         x: 2000,
+//         y: 300,
+//         br: 20,
+//         ax: -20,
+//       }));
+//       this.worldDAO.put(this.TestEntity.create({
+//         id: 'testbullet2',
+//         x: 4000,
+//         y: 500,
+//         br: 20,
+//         ax: -20,
+//       }));
+//       this.worldDAO.put(this.TestEntity.create({
+//         id: 'testbullet3',
+//         x: 6000,
+//         y: 400,
+//         br: 20,
+//         ax: -20,
+//       }));
+//       this.worldDAO.put(this.TestEntity.create({
+//         id: 'testbullet4',
+//         x: 7000,
+//         y: 200,
+//         br: 20,
+//         ax: -20,
+//       }));
+//       this.worldDAO.put(this.TestEntity.create({
+//         id: 'testbullet5',
+//         x: 8000,
+//         y: 300,
+//         br: 20,
+//         ax: -20,
+//       }));
+//
+//
+//       // make views
+//       var views = this.ArraySink.create();
+//       this.worldDAO.select(this.Map.create({ f: this.Entity.SPRITE.f, delegate: views }));
+//       this.canvas.cview.children = views.a;
 
       // insert canvas
       document.body.innerHTML = this.canvas.toHTML() + document.body.innerHTML;
@@ -149,17 +164,6 @@ foam.CLASS({
       isFramed: true,
       code: function() {
         this.time = Date.now();
-        //this.canvas.cview.invalidated.publish();
-
-//         var self = this;
-//         this.worldDAO.select({
-//           put: function(o) {
-//             o.x += Math.random() * 2 - 1;
-//             o.y += Math.random() * 2 - 1;
-//             o.rotation += Math.random() * 0.02 - 0.01;
-//             self.worldDAO.put(o); // TODO: automate putting back in a framed listener
-//           }
-//         });
       }
     },
     {
--- src/Player.js
+++ src/Player.js
@@ -14,22 +14,77 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
+foam.CLASS({
+  package: 'tabletop',
+  name: 'PlayerEntity',
+  implements: ['tabletop.Entity' ],
+  requires: [ 'tabletop.TestSprite' ],
+  imports: ['canvas'],
+  properties: [
+    {
+      name: 'sprite',
+      factory: function() {
+        this.propertyChange.subscribe(this.updateSprite);
+        var s = this.TestSprite.create({
+          x: this.x,
+          y: this.y,
+          rotation: this.rotation
+        });
+        this.canvas.get().cview.children.push(s);
+        this.canvas.get().cview.addChild_(s);
+        return s;
+      }
+    }
+  ],
+  listeners: [
+    {
+      /** This is standing in for buggy direct bindings, though being framed is handy */
+      name: 'updateSprite',
+      //isFramed: true, // ends up taking too much time
+      code: function() {
+        var s = this.sprite;
+        s.x = this.x;
+        s.y = this.y;
+        s.rotation = this.rotation;
+      }
+    }
+  ]
+});
+
+
 foam.CLASS({
   package: 'tabletop',
   name: 'PlayerManager',
   extends: 'tabletop.EntityManager',
   requires: [
+    'tabletop.PlayerEntity',
   ],
   imports: [
     'worldDAO',
-    'time',
   ],
   properties: [
+    {
+      name: 'main',
+      factory: function() {
+        return this.PlayerEntity.create();
+      }  
+    },
+    {
+      name: 'corner',
+      factory: function() { return [0,0]; },
+      postSet: function(old,nu) {
+        this.main.x = nu[0] ? 1000 - 20 : 20;
+        this.main.y = nu[1] ? 700 - 20 : 20;
+      }
+    }
+    
   ],
   methods: [
     function init() {
-      
+      this.worldDAO.get().put(this.main);
+      this.corner = this.corner;
+      this.main.sprite;
     }
   ],  
 });
