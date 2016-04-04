3ae12690ad4b54295c0e633d2138b9f223f21425
Jackson Stone <jacksonic@google.com>
2016-04-04 09:44:54 -0400

    Cleaned up object pool

--- src/core/Pool.js
+++ src/core/Pool.js
@@ -20,32 +20,34 @@ foam.CLASS({
   package: 'foam.pattern',
   name: 'Pooled',
   axioms: [ foam.pattern.Singleton.create() ],
-  
+
   methods: [
     function installInClass(cls) {
-      // TODO: global for now, but could be anywhere that is 'per-process' and can
-      // be accessed for clearing out pools when desired
-      if ( ! foam.__FOAM_objectPools__ ) { foam.__FOAM_objectPools__ = {}; }
-      var pool = foam.__FOAM_objectPools__[cls];
+      // Keeping the object pools in an accessible location allows them
+      // to be cleared out.
+      if ( ! foam.__objectPools__ ) { foam.__objectPools__ = {}; }
+      var pool = foam.__objectPools__[cls];
       if ( ! pool ) {
-        pool = foam.__FOAM_objectPools__[cls] = [];
+        pool = foam.__objectPools__[cls] = [];
       }
-      
+
       var oldCreate = cls.create;
       cls.create = function(args, X) {
-      
         var nu;
+        // Pull from the pool, run the usual init process that .create() would
+        // do. TODO: Alter create to accept the base object so we don't duplicate
+        // this init code?
         if ( pool.length ) {
           nu = pool.splice(-1, 1)[0];
           nu.destroyed = false;
           nu.initArgs(args, X);
           nu.init && nu.init();
         } else {
-          nu = oldCreate.apply(this, arguments); 
-        }  
+          nu = oldCreate.apply(this, arguments);
+        }
         return nu;
       }
-      
+
       var oldDestroy = cls.prototype.destroy;
       cls.prototype.destroy = function() {
         if ( this.destroyed ) return;
@@ -53,19 +55,18 @@ foam.CLASS({
         // Run destroy process on the object, but leave its privates empty but intact
         // to avoid reallocating them
         var inst_ = this.instance_;
-        var priv_ = this.private_; 
-        
+        var priv_ = this.private_;
+
         oldDestroy.apply(this, arguments);
 
         for ( var ikey in inst_ ) { delete inst_[ikey]; }
         for ( var pkey in priv_ ) { delete priv_[pkey]; }
-          
-        this.instance_ = inst_;  
+        this.instance_ = inst_;
         this.private_ = priv_;
-        
+
         // put the empty husk into the pool
         pool.push(this);
       }
     },
-  ]  
+  ]
 });
